f401c4961bf2fa5ea202b9118333baa72e01bfb5
Lucas Van den Broeck <lucas_vandenbroeck@telenet.be>
2026-04-25 11:18:03 +0200

    fix user not logging out

--- src/actions/auth/logout.ts
+++ src/actions/auth/logout.ts
@@ -2,10 +2,11 @@
 
 import { createServerSupabaseClient } from "@/lib/supabase/server/server";
 import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
 
 export const logout = async () => {
   const supabase = await createServerSupabaseClient();
   await supabase.auth.signOut();
-  revalidatePath("/");
-  return { redirectTo: "/" };
+  revalidatePath("/", "layout");
+  redirect("/");
 };
